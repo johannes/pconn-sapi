2c82f5dcbac10f6af19456febed5a35d32adabb8
Johannes Schlüter <johannes@schlueters.de>
2009-07-14 00:31:53 +0200

    Add support for startup and shutdown files

--- main.c
+++ main.c
@@ -13,27 +13,38 @@ Author: Johannes Schlüter
 
 #include "pconnect-sapi.h"
 
-void run_php(int iterations, char *filename)
+void run_php(char *startup, char *shutdown, int iterations, char *filename)
 {
 	pconn_init_php();
+	if (startup)
+		pconn_do_request(startup);
 	while (iterations--) {
 		pconn_do_request(filename);
 	}
+	if (shutdown)
+		pconn_do_request(shutdown);
 	pconn_shutdown_php();
 }
 
 void usage(char *name)
 {
-	fprintf(stderr, "Usage: %s [-i iterations] script\n", name);
+	fprintf(stderr, "Usage: %s [-i iterations] [-a startup_script] [-z shutdown-script] script\n", name);
 }
 
 int main(int argc, char *argv[])
 {
 	int iterations = 2;
 	int opt;
+	char *startup_script = NULL, *shutdown_script = NULL;
  
-	while ((opt = getopt(argc, argv, "hi:")) != -1) {
+	while ((opt = getopt(argc, argv, "hi:a:z:")) != -1) {
 		switch (opt) {
+		case 'a':
+			startup_script = optarg;
+			break;
+		case 'z':
+			shutdown_script = optarg;
+			break;
 		case 'i':
 			iterations = atoi(optarg);
 			if (iterations > 1) {
@@ -54,7 +65,7 @@ int main(int argc, char *argv[])
 		return 1;
 	}
 
-	run_php(iterations, argv[optind]);
+	run_php(startup_script, shutdown_script, iterations, argv[optind]);
 	return 0;
 }
 
