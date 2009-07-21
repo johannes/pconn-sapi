a8ef31dece5ff24618274f72d46c7764502f65d9
johannes <johannes@guybrush.(none)>
2009-07-22 01:09:13 +0200

    Restructure some stuff, make it cleaner

--- main.c
+++ main.c
@@ -13,32 +13,38 @@ Author: Johannes Schlüter
 
 #include "pconnect-sapi.h"
 
-char *filename;
-char *startup_script = NULL;
-char *shutdown_script = NULL;
-int iterations;
+typedef struct {
+	int iterations;
+	char *main_script;
+	char *startup_script;
+	char *shutdown_script;
+} req_data;
 
-void run_php(TSRMLS_D)
+void run_php(req_data *data TSRMLS_DC)
 {
-	int i = iterations;
-	if (startup_script)
-		pconn_do_request(startup_script TSRMLS_CC);
-	while (i--) {
-		pconn_do_request(filename TSRMLS_CC);
+	int i = data->iterations;
+	if (data->startup_script) {
+		pconn_do_request(data->startup_script TSRMLS_CC);
 	}
-	if (shutdown_script)
-		pconn_do_request(shutdown_script TSRMLS_CC);
+	while (i--) {
+		pconn_do_request(data->main_script TSRMLS_CC);
+		usleep(1);
+	}
+	if (data->shutdown_script) {
+		pconn_do_request(data->shutdown_script TSRMLS_CC);
+	}
 }
 
 #ifdef ZTS
 void *php_thread(void *arg)
 {
+	req_data *data = (req_data *)arg;
 	TSRMLS_FETCH();
-	run_php(TSRMLS_C);
+	run_php(data TSRMLS_CC);
 	return NULL;
 }
 
-void run_threads(int concurrency)
+void run_threads(req_data *data, int concurrency)
 {
 	int i;
 	/* TODO: LIMIT!!!! */
@@ -46,7 +52,7 @@ void run_threads(int concurrency)
 
 	
 	for (i=0; i < concurrency && i < 100; i++) {
-		pthread_create(&threads[i], NULL, php_thread, NULL);
+		pthread_create(&threads[i], NULL, php_thread, data);
 	}
 	
 	for (i=0; i < concurrency && i < 100; i++) {
@@ -55,18 +61,20 @@ void run_threads(int concurrency)
 }
 #endif
 
-void usage(char *name)
+void usage(char *name, int status)
 {
-	fprintf(stderr, "Usage: %s [-c <iterations>] [-a <startup>] [-z <shutdown>] <script>\n", name);
+	fprintf(stderr, "Usage: %s [-n iterations] [-c <concurrency>] [-a <startup>] [-z <shutdown>] <script>\n", name);
 	fprintf(stderr, "       %s -i\n\n", name);
 	fprintf(stderr, "  -i               Print phpinfo();\n");
 	fprintf(stderr, "  -n <iterations>  Set number of iterations (default=2)\n");
 #ifdef ZTS
-	fprintf(stderr, "  -c <concurrency> Set the number of concurrent threads\n");
+	fprintf(stderr, "  -c <concurrency> Set the number of concurrent threads (def=1)\n");
 #endif
 	fprintf(stderr, "  -a <startup>     Startup script, run once on start\n");
 	fprintf(stderr, "  -z <shutdown>    Shutdown script, executed one on end\n");
 	fprintf(stderr, "  <script>         Main script to be executed multiple times\n\n");
+
+	exit(status);
 }
 
 int main(int argc, char *argv[])
@@ -75,6 +83,7 @@ int main(int argc, char *argv[])
 	int concurrency = 1;
 #endif
 	int opt;
+	req_data data = { 2, NULL, NULL, NULL };
  
 	while ((opt = getopt(argc, argv, "hin:c:a:z:")) != -1) {
 		switch (opt) {
@@ -82,10 +91,10 @@ int main(int argc, char *argv[])
 			pconn_phpinfo();
 			return 0;
 		case 'a':
-			startup_script = optarg;
+			data.startup_script = optarg;
 			break;
 		case 'z':
-			shutdown_script = optarg;
+			data.shutdown_script = optarg;
 			break;
 #ifdef ZTS
 		case 'c':
@@ -93,32 +102,29 @@ int main(int argc, char *argv[])
 			break;
 #endif
 		case 'n':
-			iterations = atoi(optarg);
-			if (iterations > 0) {
-				break;
+			data.iterations = atoi(optarg);
+			if (data.iterations == 0) {
+				usage(argv[0], 1); /*terminates */
 			}
-			/* fall through */
+			break;
 		default:
-			usage(argv[0]);
-			return 1;
+			usage(argv[0], 1); /* terminates */
 		case 'h':		
-			usage(argv[0]);
-			return 0;
+			usage(argv[0], 0); /* terminates */
 		}
 	}
 
 	if (optind >= argc) {
-		usage(argv[0]);
-		return 1;
+		usage(argv[0], 1); /* termina */
 	}
 
-	filename = argv[optind];
+	data.main_script = argv[optind];
 
 	pconn_init_php();
 #ifdef ZTS
-	run_threads(concurrency);
+	run_threads(&data, concurrency);
 #else
-	run_php();
+	run_php(&data);
 #endif
 	pconn_shutdown_php();
 	return 0;
